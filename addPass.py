0f44823a7c2934cfe1ad148139b33591d30ea2a3
dhairyasd <54510612+dhairyasd@users.noreply.github.com>
2020-03-16 21:18:30 -0400

    Final Commit Add variable to add path of the config folder, place the python file just outside the config folder and run with administrator permissions if the config file is inside the program files

--- addPass.py
+++ addPass.py
@@ -4,22 +4,24 @@ Created on Mon Mar 16 16:34:17 2020
 
 @author: Dhairya's Laptop
 """
-
+PATH_OF_CONFIG = "C:/Program Files/OpenVPN"
 import glob
 import os
-files = glob.glob("C:/Program Files/OpenVPN/config/*.ovpn")
+files = glob.glob(PATH_OF_CONFIG+"/config/*.ovpn")
 for x in range(0,len(files)):
     f = open(files[x],'r')
     t = open("temp.txt",'w')
     flag = 0
     for line in f:
-        if "" in line:
+        if "block-outside-dns" in line:
             flag = 1
             break
+    f.seek(0)
     for line in f:
         if "auth-user-pass" in line:
-            t.write("auth-user-pass pass.txt")
-            t.write("")
+            t.write("auth-user-pass pass.txt\n")
+            if flag == 0: 
+                t.write("block-outside-dns\n")
         else:
             t.write(line)
     f.close()
